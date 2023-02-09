d867f00c4fbdfa11f5070909d5298b9af55261f7
IamChetang <96056914+IamPoojaG@users.noreply.github.com>
2023-02-09 22:55:54 +0530

    added api to chartjs

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,10 @@
 import './App.css';
-import { Chart as ChartJS } from 'chart.js/auto';
-
-import { Bar } from 'react-chartjs-2';
+import BarChart from './chart/BarChart.js';
 
 function App() {
-  const staticData = [
-    {
-      label: 'Store 1',
-      data: [100, 150, 200, 250, 300, 350],
-      backgroundColor: 'red',
-      barThickness: 12,
-    },
-    {
-      label: 'Store 2',
-      data: [321, 212, 344, 332, 223, 466],
-      backgroundColor: 'green',
-      barThickness: 12,
-    },
-    {
-      label: 'Store 3',
-      data: [123, 200, 232, 600, 232, 223],
-      backgroundColor: 'orange',
-      barThickness: 12,
-    },
-    {
-      label: 'Store 4',
-      data: [100, 500, 300, 343, 143, 343],
-      backgroundColor: 'purple',
-      barThickness: 12,
-    },
-  ];
   return (
     <div className='App'>
-      <h1>Welcome to Bar Chart</h1>
-      <Bar
-        data={{
-          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
-          datasets: staticData,
-        }}
-      ></Bar>
+      <BarChart />
     </div>
   );
 }
---
+++ client/src/chart/BarChart.js
@@ -0,0 +1,79 @@
+import { Chart as ChartJS, BarElement } from 'chart.js/auto';
+import React, { useState, useEffect } from 'react';
+
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(BarElement);
+
+const BarChart = () => {
+  const [chart, setChart] = useState({});
+  var baseUrl = 'https://api.coinranking.com/v2/coins/?limit=10';
+  var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  var apiKey = 'coinranking4ab94fbf0d5ba26d6178ee2f48db2a4fac08db2d9dfdc496';
+
+  useEffect(() => {
+    const fetchStats = async () => {
+      await fetch(`${proxyUrl}${baseUrl}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-access-token': `${apiKey}`,
+          'Access-Control-Allow-Origin': '*',
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            response.json().then((json) => {
+              console.log(json.data);
+              setChart(json.data);
+            });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    fetchStats();
+  }, [baseUrl, proxyUrl, apiKey]);
+
+  console.log('chart', chart);
+
+  const staticData = [
+    {
+      label: 'Store 1',
+      data: chart?.coins?.map((x) => x.price),
+      backgroundColor: 'red',
+      barThickness: 12,
+    },
+    // {
+    //   label: 'Store 2',
+    //   data: chart?.coins?.map((x) => x.price),
+    //   backgroundColor: 'green',
+    //   barThickness: 12,
+    // },
+    // {
+    //   label: 'Store 3',
+    //   data: chart?.coins?.map((x) => x.price),
+    //   backgroundColor: 'orange',
+    //   barThickness: 12,
+    // },
+    // {
+    //   label: 'Store 4',
+    //   data: chart?.coins?.map((x) => x.price),
+    //   backgroundColor: 'purple',
+    //   barThickness: 12,
+    // },
+  ];
+  return (
+    <div className='App'>
+      <h1>Welcome to Bar Chart</h1>
+      <Bar
+        data={{
+          labels: chart?.coins?.map((x) => x.name),
+          datasets: staticData,
+        }}
+      ></Bar>
+    </div>
+  );
+};
+export default BarChart;
